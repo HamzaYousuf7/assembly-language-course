9c3a9bc08226e33a7acd81c30bb38eba2ae983fb
Hamza <hamzayousuf175@gmail.com>
2020-11-07 18:01:16 +0500

    added question 1

--- Question 1/Question1.asm
+++ Question 1/Question1.asm
@@ -4,6 +4,14 @@
 .code
 
 main proc
-
+mov bl,50 ;50 represent 2 in ascii code
+mov cl,3
+add bl,cl ;adding 3 which will give us 5
+mov dl,bl ; mov bl value so we can print
+mov ah,2 ;printing 
+int 21h
+mov ah,4ch
+int 21h
 main endp
 end main
+
